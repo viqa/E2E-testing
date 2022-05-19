f8277d956a09af6afd8f305da7e597bed9695d21
viqa <vinicius@upoutsourcing.com>
2022-05-19 14:06:13 -0300

    5 e2e

--- cypress/integration/home-page.spec.js
+++ cypress/integration/home-page.spec.js
@@ -1,6 +1,7 @@
+//those elements are suppose to have data-cy tags on them, so the test would be more assertive and the code cleaner
+
 describe('E2E tests', () => {
     it('Home page', () => {
-        //those elements are suppose to have data-cy tags on them, so the test would be more assertive
         cy.visit('/');
         cy.get('.home').should('be.visible')
             .should('contain', 'Home')
@@ -17,5 +18,54 @@ describe('E2E tests', () => {
         cy.get('form.ng-valid > .btn').click()
         cy.get('.fontBig').should('have.text', 'Hermoine Granger')
         cy.get('.borderM > :nth-child(3)').should('have.text','Account Number : 1001 , \n\tBalance : 5096 , \n\tCurrency : Dollar')
+    })
+
+    it('Transactions', () => {
+        cy.get('[ng-class="btnClass1"]').click()
+        cy.get('[style="float:right;margin-top:-30px;"]').click()
+        cy.get('.fixedTopBox > [style="float:left"]').click()
+        cy.get('[ng-class="btnClass2"]').click()
+        cy.get('label').should('have.text', 'Amount to be Deposited :')
+        cy.get('.form-control').type('1000')
+        cy.get('form.ng-dirty > .btn').click()
+        cy.get('.error').should('have.text', 'Deposit Successful')//this element shoud not be an error class, but a success class
+        cy.wait(1000) //ideally we should wait for a request or element, but this will work
+        cy.get('[ng-class="btnClass1"]').click()
+        cy.get("#anchor0 > :nth-child(2)").then(($el) => {
+            const text = $el.text(); 
+
+            expect(text).to.eq("1000");
+          });
+        cy.get('.logout').click()
+        cy.get('.home').click()
+    })
+
+    it('Bank Manager Login', () => {
+        cy.get(':nth-child(3) > .btn').click()
+        cy.get('[ng-class="btnClass1"]').should('have.text', 'Add Customer\n\t\t').click()
+        cy.get(':nth-child(1) > .form-control').type('Michael')
+        cy.get(':nth-child(2) > .form-control').type('Scott')
+        cy.get(':nth-child(3) > .form-control').type('18503')
+        cy.get('form.ng-dirty > .btn').click()
+        cy.get('[ng-class="btnClass3"]').should('have.text','Customers\n\t\t').click()
+        cy.get(":nth-child(6) > :nth-child(1)").then(($el) => {
+            const text = $el.text(); 
+
+            expect(text).to.eq("Michael");
+          })
+
+    })
+
+    it('Delete Customer', () => {
+        cy.get(':nth-child(1) > :nth-child(5) > button').click()
+        cy.get(':nth-child(1) > :nth-child(5) > button').click()
+        cy.get(':nth-child(1) > :nth-child(5) > button').click()
+        cy.get(':nth-child(1) > :nth-child(5) > button').click()
+        cy.get(':nth-child(1) > :nth-child(5) > button').click() //a little dumb, but it works ^^ 
+        cy.get("tbody > .ng-scope > :nth-child(1)").then(($el) => {
+            const text = $el.text(); 
+
+            expect(text).to.eq("Michael");
+          })
     })
 })
