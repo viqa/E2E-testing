bd96b6df5c63d853922635a42361ae6b5dfcc4b1
viqa <vinicius@upoutsourcing.com>
2022-05-19 14:28:38 -0300

    e2e tests

--- cypress/integration/home-page.spec.js
+++ cypress/integration/home-page.spec.js
@@ -1,6 +1,7 @@
 //those elements are suppose to have data-cy tags on them, so the test would be more assertive and the code cleaner
 
 describe('E2E tests', () => {
+
     it('Home page', () => {
         cy.visit('/');
         cy.get('.home').should('be.visible')
@@ -25,20 +26,37 @@ describe('E2E tests', () => {
         cy.get('[style="float:right;margin-top:-30px;"]').click()
         cy.get('.fixedTopBox > [style="float:left"]').click()
         cy.get('[ng-class="btnClass2"]').click()
+        //first we deposit some money
         cy.get('label').should('have.text', 'Amount to be Deposited :')
         cy.get('.form-control').type('1000')
         cy.get('form.ng-dirty > .btn').click()
         cy.get('.error').should('have.text', 'Deposit Successful')//this element shoud not be an error class, but a success class
         cy.wait(1000) //ideally we should wait for a request or element, but this will work
+        //now we confirm we have the right amount on account
         cy.get('[ng-class="btnClass1"]').click()
         cy.get("#anchor0 > :nth-child(2)").then(($el) => {
+            const text = $el.text()
+
+            expect(text).to.eq("1000")
+          })
+        cy.get('.fixedTopBox > [style="float:left"]').click()
+        //now we withdraw some money 
+        cy.get('[ng-class="btnClass3"]').click()
+        cy.get('.form-control').type('500')
+        cy.get('form.ng-dirty > .btn').click()
+        cy.get('.error').should('have.text', 'Transaction successful')
+        .wait(1000)
+        cy.get('[ng-class="btnClass1"]').click()
+        cy.get('#anchor1 > :nth-child(2)').then(($el) => {
             const text = $el.text(); 
 
-            expect(text).to.eq("1000");
-          });
+            expect(text).to.eq("500");
+          })
+    
         cy.get('.logout').click()
         cy.get('.home').click()
     })
+
 
     it('Bank Manager Login', () => {
         cy.get(':nth-child(3) > .btn').click()
